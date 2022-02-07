e09d19039e781908adff8dcdaee5d5a04fb43d80
Rushikesh Lade <rushikesh.lade@mobisoftinfotech.com>
2022-02-07 16:17:11 +0530

    sync event dto class creation

---
+++ lib/models/data_models/sync_event_menu.dart
@@ -0,0 +1,297 @@
+
+
+class SyncEventMenu {
+  SyncEventMenu({
+    required this.pOsSyncEventDataDtoList,
+    required this.pOsSyncItemCategoryDataDtoList,
+    required this.pOsSyncEventItemDataDtoList,
+    required this.pOsSyncEventItemExtrasDataDtoList,
+    required this.pOsSyncDeletedEventDataDtoList,
+    required this.pOsSyncDeletedItemCategoryDataDtoList,
+    required this.pOsSyncDeletedEventItemDataDtoList,
+    required this.pOsSyncDeletedEventItemExtrasDataDtoList,
+  });
+
+  List<POsSyncEventDataDtoList> pOsSyncEventDataDtoList;
+  List<POsSyncItemCategoryDataDtoList> pOsSyncItemCategoryDataDtoList;
+  List<POsSyncEventItemDataDtoList> pOsSyncEventItemDataDtoList;
+  List<POsSyncEventItemExtrasDataDtoList> pOsSyncEventItemExtrasDataDtoList;
+  List<POsSyncEventDataDtoList> pOsSyncDeletedEventDataDtoList;
+  List<POsSyncItemCategoryDataDtoList> pOsSyncDeletedItemCategoryDataDtoList;
+  List<POsSyncEventItemDataDtoList> pOsSyncDeletedEventItemDataDtoList;
+  List<POsSyncEventItemExtrasDataDtoList> pOsSyncDeletedEventItemExtrasDataDtoList;
+
+  factory SyncEventMenu.fromJson(Map<String, dynamic> json) => SyncEventMenu(
+    pOsSyncEventDataDtoList: List<POsSyncEventDataDtoList>.from(json["pOSSyncEventDataDtoList"].map((x) => POsSyncEventDataDtoList.fromJson(x))),
+    pOsSyncItemCategoryDataDtoList: List<POsSyncItemCategoryDataDtoList>.from(json["pOSSyncItemCategoryDataDtoList"].map((x) => POsSyncItemCategoryDataDtoList.fromJson(x))),
+    pOsSyncEventItemDataDtoList: List<POsSyncEventItemDataDtoList>.from(json["pOSSyncEventItemDataDtoList"].map((x) => POsSyncEventItemDataDtoList.fromJson(x))),
+    pOsSyncEventItemExtrasDataDtoList: List<POsSyncEventItemExtrasDataDtoList>.from(json["pOSSyncEventItemExtrasDataDtoList"].map((x) => POsSyncEventItemExtrasDataDtoList.fromJson(x))),
+    pOsSyncDeletedEventDataDtoList: List<POsSyncEventDataDtoList>.from(json["pOSSyncDeletedEventDataDtoList"].map((x) => POsSyncEventDataDtoList.fromJson(x))),
+    pOsSyncDeletedItemCategoryDataDtoList: List<POsSyncItemCategoryDataDtoList>.from(json["pOSSyncDeletedItemCategoryDataDtoList"].map((x) => POsSyncItemCategoryDataDtoList.fromJson(x))),
+    pOsSyncDeletedEventItemDataDtoList: List<POsSyncEventItemDataDtoList>.from(json["pOSSyncDeletedEventItemDataDtoList"].map((x) => POsSyncEventItemDataDtoList.fromJson(x))),
+    pOsSyncDeletedEventItemExtrasDataDtoList: List<POsSyncEventItemExtrasDataDtoList>.from(json["pOSSyncDeletedEventItemExtrasDataDtoList"].map((x) => POsSyncEventItemExtrasDataDtoList.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "pOSSyncEventDataDtoList": List<dynamic>.from(pOsSyncEventDataDtoList.map((x) => x.toJson())),
+    "pOSSyncItemCategoryDataDtoList": List<dynamic>.from(pOsSyncItemCategoryDataDtoList.map((x) => x.toJson())),
+    "pOSSyncEventItemDataDtoList": List<dynamic>.from(pOsSyncEventItemDataDtoList.map((x) => x.toJson())),
+    "pOSSyncEventItemExtrasDataDtoList": List<dynamic>.from(pOsSyncEventItemExtrasDataDtoList.map((x) => x.toJson())),
+    "pOSSyncDeletedEventDataDtoList": List<dynamic>.from(pOsSyncDeletedEventDataDtoList.map((x) => x.toJson())),
+    "pOSSyncDeletedItemCategoryDataDtoList": List<dynamic>.from(pOsSyncDeletedItemCategoryDataDtoList.map((x) => x.toJson())),
+    "pOSSyncDeletedEventItemDataDtoList": List<dynamic>.from(pOsSyncDeletedEventItemDataDtoList.map((x) => x.toJson())),
+    "pOSSyncDeletedEventItemExtrasDataDtoList": List<dynamic>.from(pOsSyncDeletedEventItemExtrasDataDtoList.map((x) => x.toJson())),
+  };
+}
+
+class POsSyncEventDataDtoList {
+  POsSyncEventDataDtoList({
+    required this.eventId,
+    required this.eventCode,
+    required this.addressLine1,
+    required this.addressLine2,
+    required this.city,
+    required this.state,
+    required this.country,
+    required this.zipCode,
+    required this.startDateTime,
+    required this.endDateTime,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.createdBy,
+    required this.updatedBy,
+    required this.deleted,
+  });
+
+  String eventId;
+  String eventCode;
+  String addressLine1;
+  String addressLine2;
+  String city;
+  String state;
+  String country;
+  String zipCode;
+  int startDateTime;
+  int endDateTime;
+  int createdAt;
+  int updatedAt;
+  String createdBy;
+  String updatedBy;
+  bool deleted;
+
+  factory POsSyncEventDataDtoList.fromJson(Map<String, dynamic> json) => POsSyncEventDataDtoList(
+    eventId: json["eventId"],
+    eventCode: json["eventCode"],
+    addressLine1: json["addressLine1"],
+    addressLine2: json["addressLine2"],
+    city: json["city"],
+    state: json["state"],
+    country: json["country"],
+    zipCode: json["zipCode"],
+    startDateTime: json["startDateTime"],
+    endDateTime: json["endDateTime"],
+    createdAt: json["createdAt"],
+    updatedAt: json["updatedAt"],
+    createdBy: json["createdBy"],
+    updatedBy: json["updatedBy"],
+    deleted: json["deleted"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "eventId": eventId,
+    "eventCode": eventCode,
+    "addressLine1": addressLine1,
+    "addressLine2": addressLine2,
+    "city": city,
+    "state": state,
+    "country": country,
+    "zipCode": zipCode,
+    "startDateTime": startDateTime,
+    "endDateTime": endDateTime,
+    "createdAt": createdAt,
+    "updatedAt": updatedAt,
+    "createdBy": createdBy,
+    "updatedBy": updatedBy,
+    "deleted": deleted,
+  };
+}
+
+class POsSyncEventItemDataDtoList {
+  POsSyncEventItemDataDtoList({
+    required this.itemCategoryId,
+    required this.eventId,
+    required this.itemId,
+    required this.itemCode,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.soldQty,
+    required this.compQty,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.createdBy,
+    required this.updatedBy,
+    required this.deleted,
+  });
+
+  String itemCategoryId;
+  String eventId;
+  String itemId;
+  String itemCode;
+  String name;
+  String description;
+  int price;
+  int soldQty;
+  int compQty;
+  int createdAt;
+  int updatedAt;
+  String createdBy;
+  String updatedBy;
+  bool deleted;
+
+  factory POsSyncEventItemDataDtoList.fromJson(Map<String, dynamic> json) => POsSyncEventItemDataDtoList(
+    itemCategoryId: json["itemCategoryId"],
+    eventId: json["eventId"],
+    itemId: json["itemId"],
+    itemCode: json["itemCode"],
+    name: json["name"],
+    description: json["description"],
+    price: json["price"],
+    soldQty: json["soldQty"],
+    compQty: json["compQty"],
+    createdAt: json["createdAt"],
+    updatedAt: json["updatedAt"],
+    createdBy: json["createdBy"],
+    updatedBy: json["updatedBy"],
+    deleted: json["deleted"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "itemCategoryId": itemCategoryId,
+    "eventId": eventId,
+    "itemId": itemId,
+    "itemCode": itemCode,
+    "name": name,
+    "description": description,
+    "price": price,
+    "soldQty": soldQty,
+    "compQty": compQty,
+    "createdAt": createdAt,
+    "updatedAt": updatedAt,
+    "createdBy": createdBy,
+    "updatedBy": updatedBy,
+    "deleted": deleted,
+  };
+}
+
+class POsSyncEventItemExtrasDataDtoList {
+  POsSyncEventItemExtrasDataDtoList({
+    required this.eventId,
+    required this.itemId,
+    required this.foodExtraItemId,
+    required this.itemName,
+    required this.sellingPrice,
+    required this.imageFileId,
+    required this.minQtyAllowed,
+    required this.maxQtyAllowed,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.createdBy,
+    required this.updatedBy,
+    required this.deleted,
+  });
+
+  String eventId;
+  String itemId;
+  String foodExtraItemId;
+  String itemName;
+  int sellingPrice;
+  String imageFileId;
+  int minQtyAllowed;
+  int maxQtyAllowed;
+  int createdAt;
+  int updatedAt;
+  String createdBy;
+  String updatedBy;
+  bool deleted;
+
+  factory POsSyncEventItemExtrasDataDtoList.fromJson(Map<String, dynamic> json) => POsSyncEventItemExtrasDataDtoList(
+    eventId: json["eventId"],
+    itemId: json["itemId"],
+    foodExtraItemId: json["foodExtraItemId"],
+    itemName: json["itemName"],
+    sellingPrice: json["sellingPrice"],
+    imageFileId: json["imageFileId"],
+    minQtyAllowed: json["minQtyAllowed"],
+    maxQtyAllowed: json["maxQtyAllowed"],
+    createdAt: json["createdAt"],
+    updatedAt: json["updatedAt"],
+    createdBy: json["createdBy"],
+    updatedBy: json["updatedBy"],
+    deleted: json["deleted"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "eventId": eventId,
+    "itemId": itemId,
+    "foodExtraItemId": foodExtraItemId,
+    "itemName": itemName,
+    "sellingPrice": sellingPrice,
+    "imageFileId": imageFileId,
+    "minQtyAllowed": minQtyAllowed,
+    "maxQtyAllowed": maxQtyAllowed,
+    "createdAt": createdAt,
+    "updatedAt": updatedAt,
+    "createdBy": createdBy,
+    "updatedBy": updatedBy,
+    "deleted": deleted,
+  };
+}
+
+class POsSyncItemCategoryDataDtoList {
+  POsSyncItemCategoryDataDtoList({
+    required this.eventId,
+    required this.categoryId,
+    this.categoryCode,
+    required this.categoryName,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.createdBy,
+    required this.updatedBy,
+    required this.deleted,
+  });
+
+  String eventId;
+  String categoryId;
+  dynamic categoryCode;
+  String categoryName;
+  int createdAt;
+  int updatedAt;
+  String createdBy;
+  String updatedBy;
+  bool deleted;
+
+  factory POsSyncItemCategoryDataDtoList.fromJson(Map<String, dynamic> json) => POsSyncItemCategoryDataDtoList(
+    eventId: json["eventId"],
+    categoryId: json["categoryId"],
+    categoryCode: json["categoryCode"],
+    categoryName: json["categoryName"],
+    createdAt: json["createdAt"],
+    updatedAt: json["updatedAt"],
+    createdBy: json["createdBy"],
+    updatedBy: json["updatedBy"],
+    deleted: json["deleted"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "eventId": eventId,
+    "categoryId": categoryId,
+    "categoryCode": categoryCode,
+    "categoryName": categoryName,
+    "createdAt": createdAt,
+    "updatedAt": updatedAt,
+    "createdBy": createdBy,
+    "updatedBy": updatedBy,
+    "deleted": deleted,
+  };
+}
