a35814fd29ff85f17640825f9eb671e08fa75db4
Rushikesh Lade <rushikesh.lade@mobisoftinfotech.com>
2022-03-10 12:23:48 +0530

    microblink integration

--- lib/screens/payment/payment_screen.dart
+++ lib/screens/payment/payment_screen.dart
@@ -1,3 +1,4 @@
+import 'package:blinkcard_flutter/microblink_scanner.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -57,6 +58,10 @@ class _PaymentScreenState extends State<PaymentScreen> implements
   bool isPaymentDone = false;
   int receiptMode = 1;
   String orderID = 'NA';
+
+  String _resultString = "";
+  String _fullDocumentFirstImageBase64 = "";
+  String _fullDocumentSecondImageBase64 = "";
 
   TextEditingController amountReceivedController = TextEditingController();
   TextEditingController emailController = TextEditingController();
@@ -1023,6 +1028,10 @@ class _PaymentScreenState extends State<PaymentScreen> implements
       paymentModeType = index;
       updateSelectedPaymentMode();
     });
+
+    if(paymentModeType==PaymentModeConstants.creditCard){
+      scan();
+    }
   }
 
   onTapNewOrder() {
@@ -1154,6 +1163,82 @@ class _PaymentScreenState extends State<PaymentScreen> implements
     else if (response.action == CustomerActionConst.editOrderDetails) {
       showEventMenuScreen();
     }
+  }
+
+
+  Future<void> scan() async {
+    String license;
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      license =
+      "sRwAAAEYY29tLm1vYmlzb2Z0LmNhcmRzY2FubmVyng7gcmH9+49/nKTmkyV3fnkZG1MKCAnozoSkJ5CWAyzYBbxMjKWkKqYBKLYm7SFLFrj6AFA1mbFDRtlBv9+SIE45ndHPCihlGC5qJ0ezOuSajiROBuPa/zUahE+8d8q+xJsFufUog4kNGqn+4gIXs0qMX3Vd/NJHE4FKpTknpQg/tI4EABFM+WGhEKQhpFKXXnOXOo/5EMfAneIZgmzFb0jj0MWBVcuR6D5m7LNgfw==";
+    } else if (Theme.of(context).platform == TargetPlatform.android) {
+      license =
+      "sRwAAAEYY29tLm1vYmlzb2Z0LmNhcmRzY2FubmVyng7gcmH9+49/nKTmkyV3fnkZG1MKCAnozoSkJ5CWAyzYBbxMjKWkKqYBKLYm7SFLFrj6AFA1mbFDRtlBv9+SIE45ndHPCihlGC5qJ0ezOuSajiROBuPa/zUahE+8d8q+xJsFufUog4kNGqn+4gIXs0qMX3Vd/NJHE4FKpTknpQg/tI4EABFM+WGhEKQhpFKXXnOXOo/5EMfAneIZgmzFb0jj0MWBVcuR6D5m7LNgfw==";
+    } else {
+      license = "";
+    }
+
+    var cardRecognizer = BlinkCardRecognizer();
+    cardRecognizer.returnFullDocumentImage = true;
+
+    BlinkCardOverlaySettings settings = BlinkCardOverlaySettings();
+
+    var results = await MicroblinkScanner.scanWithCamera(
+        RecognizerCollection([cardRecognizer]), settings, license);
+
+    if (!mounted) return;
+
+    if (results.length == 0) return;
+    for (var result in results) {
+      if (result is BlinkCardRecognizerResult) {
+        _resultString = getCardResultString(result);
+
+        setState(() {
+          _resultString = _resultString;
+          _fullDocumentFirstImageBase64 =
+              result.firstSideFullDocumentImage ?? "";
+          _fullDocumentSecondImageBase64 =
+              result.secondSideFullDocumentImage ?? "";
+        });
+
+        return;
+      }
+    }
+  }
+
+  String getCardResultString(BlinkCardRecognizerResult result) {
+    return buildResult(result.cardNumber, 'Card Number') +
+        buildResult(result.cardNumberPrefix, 'Card Number Prefix') +
+        buildResult(result.iban, 'IBAN') +
+        buildResult(result.cvv, 'CVV') +
+        buildResult(result.owner, 'Owner') +
+        buildResult(result.cardNumberValid.toString(), 'Card Number Valid') +
+        buildDateResult(result.expiryDate, 'Expiry date');
+  }
+
+  String buildResult(String? result, String propertyName) {
+    if (result == null || result.isEmpty) {
+      return "";
+    }
+
+    return propertyName + ": " + result + "\n";
+  }
+
+  String buildDateResult(Date? result, String propertyName) {
+    if (result == null || result.year == 0) {
+      return "";
+    }
+
+    return buildResult(
+        "${result.day}.${result.month}.${result.year}", propertyName);
+  }
+
+  String buildIntResult(int? result, String propertyName) {
+    if (result == null || result < 0) {
+      return "";
+    }
+
+    return buildResult(result.toString(), propertyName);
   }
 
 }
