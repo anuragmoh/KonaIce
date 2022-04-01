8fe4517ad6852ace8be87ec3cdbcf701e042c022
gajanangarkar <gajanan.garkar@mobisoftinfotech.com>
2022-04-01 10:17:22 +0530

    Session routing issue resolve

--- lib/utils/function_utils.dart
+++ lib/utils/function_utils.dart
@@ -16,6 +16,7 @@ import 'package:kona_ice_pos/models/data_models/session.dart';
 import 'package:kona_ice_pos/screens/login/login_model.dart';
 import 'package:kona_ice_pos/screens/login/login_screen.dart';
 import 'package:kona_ice_pos/screens/splash/splash_screen.dart';
+import 'package:kona_ice_pos/utils/ServiceNotifier.dart';
 import 'package:kona_ice_pos/utils/common_widgets.dart';
 
 class FunctionalUtils {
@@ -170,6 +171,8 @@ class FunctionalUtils {
   static clearSessionData() async {
     debugPrint("getting 401 in base client");
     CommonWidgets().showErrorSnackBar(errorMessage: StringConstants.errorSessionExpire, context: navigatorKey.currentContext!);
+    final service = ServiceNotifier();
+    service.increment(0);
     Future.delayed(
         const Duration(seconds: 3),
             () async
