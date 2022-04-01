77f923573bf7d6b62ea6cf0932369f6eee50f550
gajanangarkar <gajanan.garkar@mobisoftinfotech.com>
2022-04-01 10:31:17 +0530

    Amount received issue fixed

--- lib/screens/payment/payment_screen.dart
+++ lib/screens/payment/payment_screen.dart
@@ -279,21 +279,23 @@ class _PaymentScreenState extends State<PaymentScreen>
                                   child: Center(
                                     child: Padding(
                                       padding: const EdgeInsets.only(
-                                          left: 8.0, bottom: 2.0),
+                                          left: 4.0, bottom: 2.0),
                                       child: TextField(
+                                        maxLength: TextFieldLengthConstant.amountReceived,
                                         controller: amountReceivedController,
                                         style: StyleConstants.customTextStyle(
-                                            fontSize: 22.0,
+                                            fontSize: 20.0,
                                             color: getMaterialColor(
                                                 AppColors.textColor1),
                                             fontFamily:
                                             FontConstants.montserratMedium),
-                                        keyboardType: TextInputType.number,
+                                        keyboardType: const TextInputType.numberWithOptions(decimal: true),
                                         inputFormatters: <TextInputFormatter>[
-                                          FilteringTextInputFormatter.digitsOnly
+                                          FilteringTextInputFormatter.allow(RegExp(r"[0-9.]")),
                                         ],
                                         decoration: const InputDecoration(
                                           border: InputBorder.none,
+                                          counterText: "",
                                         ),
                                         onChanged: (value) {
                                           if (value.isNotEmpty) {
